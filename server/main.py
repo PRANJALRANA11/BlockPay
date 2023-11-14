7f1dee9e3725b06d9f4086e6feff9b59d9124d07
DeepeshKalura <deepeshkalurs@gmail.com>
2023-11-14 12:44:52 +0530

    Added CORS middleware and home screen endpoint to API

--- server/main.py
+++ server/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
 # added CORS middleware to allow requests from the frontend
 origins = [
     "*"
@@ -15,3 +16,17 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+async def home_screen_of_api():
+    return {"message": "Welcome to the API of the BlockPay Project!"}
+
+
+# Now, I have to build a way to store information to the database
+@app.post("/nft")
+async def post_nft():
+    return {"message": "NFT has been stored!"}
+
+
+
+
