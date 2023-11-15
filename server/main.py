13d2d40ede0f225355792eefdc645caa5401db16
DeepeshKalura <deepeshkalurs@gmail.com>
2023-11-15 15:21:09 +0530

    Add Firebase credentials and NFT request handling

--- server/main.py
+++ server/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import firebase_admin
+from firebase_admin import firestore, credentials
+
+from server.request_and_response import NFTRequest
 
 app = FastAPI()
+cred = credentials.Certificate("server/cred/credentials.json")
+default_app = firebase_admin.initialize_app(cred)
 
 
 # added CORS middleware to allow requests from the frontend
@@ -24,7 +30,9 @@ async def home_screen_of_api():
 
 # Now, I have to build a way to store information to the database
 @app.post("/nft")
-async def post_nft():
+async def post_nft(user_request: NFTRequest):
+    # this nft reuest stores in the database then if sucessfull then return 201 response code and we got the postive response
+
     return {"message": "NFT has been stored!"}
 
 
